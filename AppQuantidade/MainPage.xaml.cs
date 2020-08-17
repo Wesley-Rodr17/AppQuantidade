3d4718022d4f86f1b6883f3cf005cbf904d39371
wes14 <Wesley-Rodr17>
2020-08-17 17:19:31 -0300

    Initial commit

---
+++ AppQuantidade/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppQuantidade
+{
+    public partial class MainPage : ContentPage
+    {
+        int quantidade = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_mais_um(object sender, EventArgs e)
+        {
+            quantidade++;
+            lblQuantidade.Text = quantidade.ToString();
+        }
+
+        private void Button_menos_um(object sender, EventArgs e)
+        {
+            
+            if (quantidade != 0)
+            {
+                quantidade--;
+                lblQuantidade.Text = quantidade.ToString();
+            }
+            
+            
+        }
+    }
+}
